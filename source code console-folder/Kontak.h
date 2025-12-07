ade256a9393179484518a9a0f03694a0743c63ac
babagas <220184901+babagas@users.noreply.github.com>
2025-12-07 13:31:41 +0700

    Add files via upload

---
+++ source code console-folder/Kontak.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  Kontak.h
+//  Implementation of the Class Kontak
+//  Created on:      30-Nov-2025 15:35:57
+//  Original author: asus
+///////////////////////////////////////////////////////////
+
+#ifndef KONTAK_H
+#define KONTAK_H
+
+#include <string>
+using namespace std;
+
+struct Kontak {
+    string nama;
+    int jumlahUang;
+};
+
+#endif
